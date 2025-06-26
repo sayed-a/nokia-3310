581c20cb65b553ec684127015af2456ef6c2884b
Abdul Sayed <abdul.sayed@standardbank.co.za>
2025-06-26 15:10:15 +0200

    Add skeleton for simulator app

---
+++ app/src/main/java/nostalgia/devices/nokia3310/apps/simulator/SimulatorApp.kt
@@ -0,0 +1,9 @@
+package nostalgia.devices.nokia3310.apps.simulator
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SimulatorApp() {
+    Text("Simulator App")
+}
