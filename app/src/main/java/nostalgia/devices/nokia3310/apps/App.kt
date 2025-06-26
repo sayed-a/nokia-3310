fa388c04ccd35468624605e160c33b7697d2d6e6
Abdul Sayed <abdul.sayed@standardbank.co.za>
2025-06-26 11:35:13 +0200

    Add caller app implementation

--- app/src/main/java/nostalgia/devices/nokia3310/apps/App.kt
+++ app/src/main/java/nostalgia/devices/nokia3310/apps/App.kt
@@ -1,6 +1,7 @@
 package nostalgia.devices.nokia3310.apps
 
 import androidx.compose.runtime.Composable
+import nostalgia.devices.nokia3310.apps.caller.CallerApp
 import nostalgia.devices.nokia3310.apps.dialler.DiallerApp
 import nostalgia.devices.nokia3310.apps.home.HomeApp
 import nostalgia.devices.nokia3310.apps.menu.MenuApp
