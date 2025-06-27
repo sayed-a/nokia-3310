129fc4ca14596d6c0f79ca35f8769eebb0352459
Abdul Sayed <abdul.sayed@standardbank.co.za>
2025-06-27 14:05:54 +0200

    Delete simulator app

--- app/src/main/java/nostalgia/devices/nokia3310/apps/simulator/SimulatorApp.kt
+++
@@ -1,9 +0,0 @@
-package nostalgia.devices.nokia3310.apps.simulator
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun SimulatorApp() {
-    Text("Simulator App")
-}
