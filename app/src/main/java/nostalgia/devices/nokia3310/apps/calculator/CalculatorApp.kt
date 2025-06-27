a1cd5dd6697b6d43dd110a0ef48e2660775ecc76
Abdul Sayed <abdul.sayed@standardbank.co.za>
2025-06-27 11:44:59 +0200

    Add skeletons for menu apps

---
+++ app/src/main/java/nostalgia/devices/nokia3310/apps/calculator/CalculatorApp.kt
@@ -0,0 +1,9 @@
+package nostalgia.devices.nokia3310.apps.calculator
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CalculatorApp() {
+    Text("Calculator App")
+}
